206f4226d2cb02bffe16c2d155c2ba7e34ebd1fc
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-05-15 00:22:06 +0700

    Register html package

--- config/app.php
+++ config/app.php
@@ -127,6 +127,7 @@ return [
         'Illuminate\Translation\TranslationServiceProvider',
         'Illuminate\Validation\ValidationServiceProvider',
         'Illuminate\View\ViewServiceProvider',
+        'Illuminate\Html\HtmlServiceProvider',
         /*
          * 3rd Party Service Providers...
          */
