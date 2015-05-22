c35d86696e9f1cb3bd08572890f0c67424859212
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-05-22 18:45:31 +0700

    Fix user model

--- app/User.php
+++ app/User.php
@@ -2,11 +2,16 @@
 
 namespace App;
 
+use App\Http\View\Presenters\UserPresenter;
+use App\Support\ModelTraits\HasSlug;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Laracasts\Presenter\PresentableTrait;
 
 /**
  * Class User
@@ -15,12 +20,13 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model implements AuthenticatableContract, CanResetPasswordContract
 {
 
-    use Authenticatable, CanResetPassword;
+    use Authenticatable, CanResetPassword, HasSlug, PresentableTrait;
 
     //--------------------------------------------------------------------------
     // Configurations
     //--------------------------------------------------------------------------
-    protected $fillable = ['name', 'email', 'password'];
+    protected $presenter = UserPresenter::class;
+    protected $fillable = ['slug', 'name', 'email', 'password'];
     protected $hidden = ['password', 'remember_token'];
 
 
@@ -32,7 +38,81 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     //--------------------------------------------------------------------------
     // Relationships
     //--------------------------------------------------------------------------
+    /**
+     * @return BelongsTo
+     */
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
 
+
+    //--------------------------------------------------------------------------
+    // Scopes
+    //--------------------------------------------------------------------------
+    /**
+     * @param Builder $query
+     *
+     * @param string  $id
+     *
+     * @return mixed
+     */
+    public function scopeFacebookId($query, $id)
+    {
+        return $query->where('facebook_id', '=', $id);
+    }
+
+
+    //--------------------------------------------------------------------------
+    // Custom Pivot Table
+    //--------------------------------------------------------------------------
+
+
+    //--------------------------------------------------------------------------
+    // Helpers
+    //--------------------------------------------------------------------------
+    /**
+     * @param string       $resource
+     * @param string|array $actions
+     *
+     * @return bool
+     */
+    public function can($resource, $actions)
+    {
+        $actions = is_array($actions) ? $actions : explode('|', $actions);
+
+        $permissions = [];
+        foreach ($this->role->permissions as $permission) {
+            $permissions[$permission['resource']][] = $permission['action'];
+        }
+
+        $deny = array_filter($actions, function ($action) use ($permissions, $resource) {
+            return ! isset($permissions[$resource]) || ! in_array($action, $permissions[$resource]);
+        });
+
+        return ! boolval($deny);
+    }
+
+    /**
+     * @param string $role
+     *
+     * @return bool
+     */
+    public function is($role)
+    {
+        return $this->role->name === $role;
+    }
+
+    /**
+     * @param string $id
+     * @param array  $columns
+     *
+     * @return Model|null
+     */
+    public static function findByFacebookId($id, $columns = ['*'])
+    {
+        return static::facebookId($id)->first($columns);
+    }
 
     //--------------------------------------------------------------------------
     // Scopes
